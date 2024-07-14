34ee91acc0ff0d5e9c8effcead1d27eba7d217a3
Apple <apple@Tasuars-MacBook-Pro.local>
2024-07-14 06:57:46 +0530

    added eureka client

--- src/main/java/com/example/springmysql/Controller/HospitalController.java
+++ src/main/java/com/example/springmysql/Controller/HospitalController.java
@@ -11,33 +11,37 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/hospital")
 public class HospitalController {
 
     @Autowired
     HospitalService hospitalService;
 
+    // Api endpoint method to get all the hospital data
     @GetMapping("/allNurses")
     public List<Hospital> getAllNurses(){
         return hospitalService.getAllNurses();
     }
-
+    // Api endpoint method to save the hospital data
     @PostMapping("/saveNurse")
     public ResponseEntity<String> saveNurse(@RequestBody Hospital hospital){
         hospitalService.saveNurse(hospital);
         return ResponseEntity.status(200).body("Nurse data has been saved");
     }
-
+//   Api endpoint method to get the nurse data by nurse first name
     @GetMapping("/getNurseByFirstName/{firstName}")
     public Optional<Hospital> getNurseByFirstName(@PathVariable String firstName){
        return hospitalService.getNurseByFirstName(firstName);
     }
 
+//    Api endpoint method to delete a nurse entry
     @DeleteMapping("/deleteNurse")
     public ResponseEntity<String> deleteNurse(@RequestBody Hospital hospital){
         hospitalService.deleteNurse(hospital);
         return ResponseEntity.status(200).body("Nurse has been deleted");
     }
 
+//    Api endpoint method to update a nurse entry in db.
     @PatchMapping("/updateNurse")
     public ResponseEntity<String> updateNurse(@RequestBody Hospital hospital){
         if (hospitalService.updateNurse(hospital)){
--- src/main/java/com/example/springmysql/service/HospitalService.java
+++ src/main/java/com/example/springmysql/service/HospitalService.java
@@ -21,25 +21,30 @@ public class HospitalService {
     @Autowired
     BCryptPasswordEncoder bCryptPasswordEncoder;
 
-
+// Method to get all the hospital data from MySql database with help of Spring data Jpa
     public List<Hospital> getAllNurses(){
       return hospitalRepo.findAll();
     }
 
+    // Method to save the hospital data in MySql database with help of Spring data Jpa.
+//    The password is encoded using Bcrypt encoder and then saved in db
     public void saveNurse(@RequestBody Hospital hospital){
         String encodedPassword = bCryptPasswordEncoder.encode(hospital.getNursePassword());
         hospital.setNursePassword(encodedPassword);
         hospitalRepo.save(hospital);
     }
 
+//    Method to get the nurse data by nurse first name
     public Optional<Hospital> getNurseByFirstName(@PathVariable String firstName){
        return hospitalRepo.getNurseByFirstName(firstName);
     }
 
+    // Method to delete a hospital entry from the db
     public void deleteNurse(@RequestBody Hospital hospital){
         hospitalRepo.delete(hospital);
     }
 
+//     Method to update hospital entry in the db
     public boolean updateNurse(@RequestBody Hospital hospital) {
         if (hospitalRepo.existsById(hospital.getNurseId())) {
             hospitalRepo.save(hospital);
